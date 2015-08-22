0804ebc2d9af8ef8dd006cdf040498302f588644
firstcommit <tetsuMac@tetsuMac-no-MacBook-Pro.local>
2015-08-22 14:30:16 +0900

    タイトル画面作成 テキストを流してプロローグ

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : DontDestroySingleton<GameManager> {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GoNextScene(string nextScene){
+		FadeManager.Instance.LoadLevel (nextScene, 1f);
+	}
+
+}
